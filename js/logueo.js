78eab7e0785296d9dc5e5e8bda8db429decdcb2f
Flavia Arroyo <flavia.e.arroyo@gmail.com>
2022-09-15 23:22:13 -0300

    pantallas de edicion

---
+++ js/logueo.js
@@ -0,0 +1,87 @@
+
+
+
+
+function registro(){
+    window.location.href="../pantallas/login.html";
+}
+
+function edicion(){
+    window.location.href="../pantallasedicion/indexEdicion.html"
+}
+
+function eliminar(){
+    Swal.fire({
+        title: 'Esta Seguro?',
+        
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, Borrar!'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Jugador Eliminado!',
+            'El jugador ya no formara parte del equipo.',
+            'success'
+          )
+        }
+      })
+
+}
+function eliminarP(){
+  Swal.fire({
+      title: 'Esta Seguro?',
+      
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Borrar!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Partido Eliminado!',
+          'ya se elimino  del partido.',
+          'success'
+        )
+      }
+    })
+
+}
+
+const wrapper = document.querySelector(".wrapper");
+         const fileName = document.querySelector(".file-name");
+         const defaultBtn = document.querySelector("#default-btn");
+         const customBtn = document.querySelector("#custom-btn");
+         const cancelBtn = document.querySelector("#cancel-btn i");
+         const img = document.querySelector(".imgjug");
+         let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+         function defaultBtnActive(){
+           defaultBtn.click();
+         }
+         defaultBtn.addEventListener("change", function(){
+           const file = this.files[0];
+           if(file){
+             const reader = new FileReader();
+             reader.onload = function(){
+               const result = reader.result;
+               img.src = result;
+               wrapper.classList.add("active");
+             }
+             cancelBtn.addEventListener("click", function(){
+               img.src = "";
+               wrapper.classList.remove("active");
+             })
+             reader.readAsDataURL(file);
+           }
+           if(this.value){
+             let valueStore = this.value.match(regExp);
+             fileName.textContent = valueStore;
+           }
+         });
+
+
+
+
